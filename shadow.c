fa76f3d0b8eb28eafa934601c5dbf364dfb18d4a
Tim Konick <konick781@gmail.com>
2014-06-01 20:03:05 -0400

    rm brackets on error checks

--- shadow.c
+++ shadow.c
@@ -44,9 +44,8 @@ char *construct_path(int pid, char *dir)
 char *pid_name(profile_t *process)
 {
     int alive = is_alive(process);
-    if (alive == -1) {
+    if (alive == -1) 
         return NULL;
-    }
     char *path = construct_path(process->pid, COMM);
     FILE *proc = fopen(path, "r");
     char *name = NULL;
@@ -115,9 +114,8 @@ int set_pid_nice(profile_t *process, int priority)
 char *get_ioprio(profile_t *process)
 {
     int ioprio = syscall(GETIOPRIO, IOPRIO_WHO_PROCESS, process->pid);
-    if (ioprio == -1) {
+    if (ioprio == -1) 
         return NULL;
-    }
     char *ioprio_class[4] = {"none/", "rt/", "be/", "idle/"};
     int nice = get_pid_nice(process);
     int ioprio_class_num = IOPRIO_CLASS(ioprio);
@@ -137,9 +135,8 @@ int set_ioprio(profile_t *process, int class, int value)
     int ioprio = IOPRIO_VALUE(class, value);
     int setioprio = syscall(SETIOPRIO, IOPRIO_WHO_PROCESS, 
                             process->pid, ioprio);
-    if (setioprio < 0) {
+    if (setioprio < 0) 
         return -1;
-    }
     return 0;
 }
 
@@ -150,17 +147,17 @@ void max_proc_res(profile_t *process, int resource, int *value)
         struct rlimit *old_limit = malloc(sizeof *old_limit);        
         struct rlimit *new_limit = NULL;
         ret = prlimit(process->pid, resource, new_limit, old_limit);
-        if (ret < 0) {
+        if (ret == -1) {
             process->max_res = (unsigned) errno;
         } else {
             process->max_res = (unsigned) old_limit->rlim_max;
-        }
+        } 
     } else {
         struct rlimit *new_limit = malloc(sizeof *new_limit);
         struct rlimit *old_limit = NULL;
         new_limit->rlim_max = *value;
         ret = prlimit(process->pid, resource, new_limit, old_limit);
-        if (ret < 0) {
+        if (ret == -1) {
             process->max_res = (unsigned) errno;
         } else {
             process->max_res = (unsigned) *value;
@@ -175,7 +172,7 @@ void cur_proc_res(profile_t *process, int resource, int *value)
         struct rlimit *old_limit = malloc(sizeof *old_limit);        
         struct rlimit *new_limit = NULL;
         ret = prlimit(process->pid, resource, new_limit, old_limit);
-        if (ret < 0) {
+        if (ret == -1) {
             process->cur_res = (unsigned) errno;
         } else {
             process->cur_res = (unsigned) old_limit->rlim_cur;
@@ -185,7 +182,7 @@ void cur_proc_res(profile_t *process, int resource, int *value)
         struct rlimit *old_limit = NULL;
         new_limit->rlim_cur = *value;
         ret = prlimit(process->pid, resource, new_limit, old_limit);
-        if (ret < 0) {
+        if (ret == -1) {
             process->cur_res = (unsigned) errno;
         } else {
             process->cur_res = (unsigned) *value;
